ee183616a583f3bb5636727a05fd763a354b1388
nik-vaibhav18 <nik.vaibhav18@gmail.com>
2024-11-26 15:15:46 +0530

    Added functions to insert data to azure cosmos and blob storage

--- src/categorizer_db.py
+++ src/categorizer_db.py
@@ -12,3 +12,34 @@ BLOB_CONNECTION_STRING=os.getenv("BLOB_CONNECTION_STRING")
 BLOB_CONTAINER_NAME="nikhilstore"
 
 
+blob_service_client=BlobServiceClient.from_connection_string(BLOB_CONNECTION_STRING)
+
+def upload_to_blob(image_path):
+    blob_client = blob_service_client.get_blob_client(container=BLOB_CONTAINER_NAME, blob=os.path.basename(image_path))
+    with open(image_path, "rb") as data:
+        content_settings = ContentSettings(content_type="image/jpeg")
+        container_client =blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
+        #container_client.create_container()
+        blob_client.upload_blob(data, overwrite=True, content_settings=content_settings)
+    return blob_client.url
+
+cosmos_client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
+
+
+def initialize_cosmos():
+    database = cosmos_client.create_database_if_not_exists(COSMOS_DATABASE_NAME)
+    container = database.create_container_if_not_exists(
+        id=COSMOS_CONTAINER_NAME,
+        partition_key=PartitionKey(path="/image_id")
+    )
+    return container
+
+
+def insert_metadata_to_cosmos(container, image_id, blob_url, gpt_response):
+    item = {
+        "id": image_id,  
+        "image_id": image_id,
+        "blob_url": blob_url,
+        "gpt_response": gpt_response
+    }
+    container.upsert_item(item)
